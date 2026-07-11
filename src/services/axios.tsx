3e04eecc7d2e8626b45c02141c6bff00e98b627f
gabriel360p <gabrielmark210@gmail.com>
2026-07-10 21:15:41 -0300

    fix:correção em variáveis de ambiente

--- src/services/axios.tsx
+++ src/services/axios.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL_BACKEND
+    baseURL: "https://topdog-backend.onrender.com"
 })
 
 export default api;
